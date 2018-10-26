b934d354f8677af0f58c0ab2708e5f202b81c49f
Sailesh Dahal <saileshbro@gmail.com>
2018-10-26 21:19:41 +0530

    event handler created

--- app.js
+++ app.js
@@ -12,5 +12,18 @@ var UIController = (function() {
 
 // Global App controller
 var controller = (function(budgetCtrl, UICtrl) {
-    document.querySelector('.add__btn')
+    var ctrlAddItem = function() {
+
+        // 1. get field inp. data
+        // 2. add item to the budget controller
+        // 3. add item to user interface
+        // 4. calculate budget
+        // 5. display the budget on the UI
+    }
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 })(budgetController, UIController);
