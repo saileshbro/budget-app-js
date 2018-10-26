f19d4d4a1f6a354ad8396ab08d33a045a5c42027
Sailesh Dahal <saileshbro@gmail.com>
2018-10-26 16:13:44 +0530

    Module created for app.js file

---
+++ app.js
@@ -0,0 +1,16 @@
+// Budget Controller
+var budgetController = (function() {
+    // some code
+})();
+
+
+// UI Controller
+var UIController = (function() {
+    // some code later
+})();
+
+
+// Global App controller
+var controller = (function(budgetCtrl, UICtrl) {
+    document.querySelector('.add__btn')
+})(budgetController, UIController);
