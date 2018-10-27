51955e8779cee5b1593d14fa8182263150811fd5
Sailesh Dahal <saileshbro@gmail.com>
2018-10-27 10:43:57 +0530

    linked modules and updated the UIcontroller

--- app.js
+++ app.js
@@ -6,23 +6,42 @@ var budgetController = (function() {
 
 // UI Controller
 var UIController = (function() {
-    // some code later
+    DOMstrings = {
+        inputType : '.add__type',
+        inputDescription : '.add__description',
+        inputValue : '.add__value',
+        inputBtn : '.add__btn'
+    }
+    return{
+        getInput : function(){
+            return{
+                type : document.querySelector(DOMstrings.inputType).value,
+                inputDescription : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    }
 })();
 
 
 // Global App controller
 var controller = (function(budgetCtrl, UICtrl) {
-    var ctrlAddItem = function() {
-
-        // 1. get field inp. data
-        // 2. add item to the budget controller
-        // 3. add item to user interface
-        // 4. calculate budget
+    var DOM = UICtrl.getDOMstrings();
+    var ctrlAddItem = function(){
+        // 1. get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+        // 2. add the item to the budget controller
+        // 3. add the item to the UI
+        // 4. calculate the budget
         // 5. display the budget on the UI
-    }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
+    };
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+    document.addEventListener('keypress',function(event){
+        if(event.keyCode===13 || event.which===13){
             ctrlAddItem();
         }
     });
